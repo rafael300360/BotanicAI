77993362bcb5191309fa6a23690dd4440fee6d7f
Rafael <rafael@example.com>
2026-04-13 17:07:37 -0300

    Arquitectura corregida: Archivo en la raiz y submodulos eliminados

---
+++ .vite/deps/react.js
@@ -0,0 +1,5 @@
+import {
+  require_react
+} from "./chunk-JMLGY4N2.js";
+import "./chunk-2TUXWMP5.js";
+export default require_react();
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/BotanicAI/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env': {},
