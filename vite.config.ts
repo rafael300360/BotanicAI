16303a16e101ba0fc1c78e795843f36072b6eabf
Rafael <rafael@example.com>
2026-04-13 17:56:37 -0300

    fix: change base path to relative for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/BotanicAI/',
+    base: './',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env': {},
